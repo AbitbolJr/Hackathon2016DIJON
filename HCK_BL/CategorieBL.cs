6ae98d7e2063f868d252eae967e63bd54f1254ac
AntMassip <a.massip@hotmail.fr>
2016-11-26 21:13:30 +0100

    Add files via upload
    Ajout fichiers BL

---
+++ HCK_BL/CategorieBL.cs
@@ -0,0 +1,32 @@
+﻿using HCK_DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCK_BL
+{
+    public class CategorieBL
+    {
+        private BaseDA baseDA;
+        public CategorieBL()
+        {
+            baseDA = new BaseDA();
+        }
+
+        public List<Categorie> GetAllCategories()
+        {
+            List<Categorie> allCategories = new List<Categorie>();
+            try
+            {
+                allCategories = baseDA.GetAll<Categorie>().ToList();
+            }
+            catch (Exception ex)
+            {
+                HCK_TOOLS.Log.WriteLog(ex);
+            }
+            return allCategories;
+        }
+    }
+}
