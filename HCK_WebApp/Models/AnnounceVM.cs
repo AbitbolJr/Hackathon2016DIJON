3135c001a3c390a8ca70e1a62a90df4c39ee1565
barros <barros@LAPTOP-NBIOT>
2016-11-27 01:19:01 +0100

    Ajout de design announce

--- HCK_WebApp/Models/AnnounceVM.cs
+++ HCK_WebApp/Models/AnnounceVM.cs
@@ -1,6 +1,7 @@
 ﻿using HCK_DAL;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -15,6 +16,7 @@ namespace HCK_WebApp.Models
         public int? idAnnonce { get; set; }
         public int? idCategorie { get; set; }
         public string titre { get; set; }
+        [DataType(DataType.MultilineText)]
         public string description { get; set; }
         public int? nbSlot { get; set; }
         public int? nbSlotUtilise { get; set; }
