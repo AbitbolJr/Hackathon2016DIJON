88d1aa42c6d5cbf4124ece9b6e38855e094e218a
barros <barros@LAPTOP-NBIOT>
2016-11-27 06:13:45 +0100

    chat

---
+++ HCK_WebApp/Controllers/ChatController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HCK_WebApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult ListMessage()
+        {
+            return View();
+        }
+
+        public ActionResult Conversation()
+        {
+            return View();
+        }
+    }
+}
